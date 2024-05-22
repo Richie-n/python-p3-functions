2c3c4e1568567921cc73b7f381bb6dd70f433506
Richard <ngangarichard335@gmail.com>
2024-05-22 13:01:33 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Richard")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("Njoroge")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(2,3))
+
 
 def halve(number):
-    pass
+    return number / 2
+print(halve(10))
